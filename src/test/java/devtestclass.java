1cd3a7b00a38b37af230e92d27440c5d66e6e981
Plator03 <muhaxheritoti@gmail.com>
2023-02-21 15:48:30 +0100

    second commit

--- src/test/java/devtestclass.java
+++ src/test/java/devtestclass.java
@@ -1,2 +1,6 @@
 public class devtestclass {
+
+    public static void main(String[] args) {
+        System.out.println("Tqifsha trepqen Tqifsha trepqen Tqifsha trepqen tqifsha torciden");
+    }
 }
