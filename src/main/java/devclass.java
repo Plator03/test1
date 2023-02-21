f2c61b74297b70bf228a2ca8962de4caefd0823f
tintii3 <96780528+tintii3@users.noreply.github.com>
2023-02-21 16:08:07 +0100

    Update devclass.java

--- src/main/java/devclass.java
+++ src/main/java/devclass.java
@@ -1,6 +1,6 @@
 public class devclass {
 
     public static void main(String[] args) {
-        System.out.println("Kurvat e prishtinen nano jon RUSPIJE, e qiqa npidh moj nane e i qim te shpija");
+        System.out.println("Mos fol palidhje");
     }
 }
